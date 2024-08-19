fe77f03aa5e9bfce772aa3c1a583412be6918543
Akari <akariondev@gmail.com>
2024-08-19 11:01:54 -0300

    anykernel: first version of any for Fogos (Motorola moto g34 5G)

--- anykernel.sh
+++ anykernel.sh
@@ -4,27 +4,34 @@
 properties() { '
 kernel.string=MoeKernel by whyakari
 do.devicecheck=1
-do.modules=0
+do.modules=1
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=ginkgo
-device.name2=willow
-supported.versions=12.0-14.0
+device.name1=fogos
+supported.versions=14.0
 supported.patchlevels=
 '; }
 
-attributes() {
-set_perm_recursive 0 0 755 644 $ramdisk/*;
-set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
-}
-
-block=/dev/block/bootdevice/by-name/boot;
-is_slot_device=0;
+block=boot;
+is_slot_device=1;
 ramdisk_compression=auto;
 patch_vbmeta_flag=auto;
 
-. tools/ak3-core.sh && attributes;
+. tools/ak3-core.sh;
+
+set_perm_recursive 0 0 755 644 $ramdisk/*;
+set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
 
 dump_boot;
 write_boot;
+
+block=vendor_boot;
+is_slot_device=1;
+ramdisk_compression=auto;
+patch_vbmeta_flag=auto;
+
+reset_ak;
+split_boot;
+
+flash_boot;
